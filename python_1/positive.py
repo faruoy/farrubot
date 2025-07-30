b34e13d889672d1c4720006c0261aad23064d216
faruoy <farhanmohammed8099@gmail.com>
2025-07-30 13:26:13 +0000

    Auto commit

--- python_1/positive.py
+++ python_1/positive.py
@@ -1,5 +1,5 @@
 #1Q) Write a program in python to demonstrate the number whether positive negative or zero 
-
+#
 #n=int(input("Enter the number:"))
 #if n>0:
 #	print(f"{n} is Positive")
@@ -21,7 +21,30 @@
 #n=int(input("Enter the Number:"))
 #guess(n)
 
-3Q) Compare and contrast list, tuple, and dictionary with examples.
-ans: Lists : Lists in python are the data types which are same like arrays in 
+#3Q) Compare and contrast list, tuple, and dictionary with examples.
+#ans: Lists[] : 
+#    Lists are ordered meaning order matters.
+#    Lists are mutable meaning we can alter in them. 
+#    Lists can take duplicate values like ex: arr=[1,3,1,3,5,6].
+#    Lists can be indexed with a number like arr[0].
+#    Uses : Lists can be used when we have to alter the data its just like array
+#Example:    
+#my_list=['book','pen','table']
+#print(my_list[0])
+#my_list[0]='pencil'
+#print(my_list[0])
+
+#Tuples() :
+#    Tuples are ordered meaning order of insertion matters
+#    Tuples are immutable meaning we cannot alter the values in tuples
+#    Tuples allows duplicate values
+#    Tuples can be indexed same like lists
+#    Uses: We can use tuples when we dont have to change the data further.
+#Example:
+my_tuple=(1,'hello',2,3)
+print(my_tuple[1])
+
+
+
 
 
