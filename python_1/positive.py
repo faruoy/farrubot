0c532557931f2cb7e828496069c71d32ac2cb620
faruoy <farhanmohammed8099@gmail.com>
2025-07-30 12:35:27 +0000

    Auto commit

---
+++ python_1/positive.py
@@ -0,0 +1,29 @@
+#1Q) Write a program in python to demonstrate the number whether positive negative or zero 
+
+#n=int(input("Enter the number:"))
+#if n>0:
+#	print(f"{n} is Positive")
+#elif n<0:
+#	print(f"{n} is Negative")
+#else:
+#	print(f"{n} is Zero")
+
+#2Q) Demonstrate how to use a user-defined function with parameters and return type in Python
+
+#def guess(n):
+#	if n>0:
+#		return print(f"{n} is Positive")
+#	elif n<0:
+#		return print(f"{n} is Negative")
+#	else: 
+#		return print(f"{n} is Zero")
+#
+#n=int(input("Enter the Number:"))
+#guess(n)
+
+3Q) Compare and contrast list, tuple, and dictionary with examples.
+
+a=[1,2,3,4]
+print(a[0])
+
+
