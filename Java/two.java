3915a06cab730422639e9fae435151a6a4134bc8
faruoy <farhanmohammed8099@gmail.com>
2025-08-22 12:39:41 +0530

    Auto commit

--- Java/two.java
+++ Java/two.java
@@ -1,5 +1,18 @@
-    public class two {
-    public static void main(String[] args) {
+  public class two {
+  public static void main(String[] args) {
         System.out.println("Hello");
     }
-}
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+
